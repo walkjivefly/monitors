5434fa8a438ba9617373df33d7c98f1dd30cf7c7
Mark Brooker <mark@walkjivefly.com>
2022-06-16 13:10:24 +0200

    Switch SCC explorer

--- height_scc.sh
+++ height_scc.sh
@@ -3,10 +3,10 @@ LOCAL=$(scc-cli getblockcount)
 if [[ $? -ne 0 ]]; then
   LOCAL="problem"
 fi
-EXPLORER="https://www.coinexplorer.net/api/v1/SCC/"
-res=$(curl -w " %{http_code}" --silent ${EXPLORER}block/latest 2>/dev/null)
+EXPLORER="http://79.143.186.234/api/"                   # Iquidus alternative
+res=$(curl -w " %{http_code}" --silent ${EXPLORER}getblockcount 2>/dev/null)
 if [[ $(echo $res|awk '{print $NF}') -eq 200 ]]; then
-  EXPLORER=$(echo $res|awk '{$NF=""; print $0}'|jq -r .result.height)
+  EXPLORER=$(echo $res|awk '{$NF=""; print $0}'|jq -r .)
 else
   EXPLORER="problem"
 fi
