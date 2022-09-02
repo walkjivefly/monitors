524982dbe03f63bbe6747b6abb71d8d51d788d8a
Mark Brooker <mark@walkjivefly.com>
2022-09-02 18:42:28 +0200

    Update for latest explorer API result format

--- height_sca.sh
+++ height_sca.sh
@@ -14,8 +14,8 @@ else
 fi
 EXPLORER="https://explorer.scalaris.info/api/"
 res=$(curl -w " %{http_code}" --silent ${EXPLORER}getblockcount 2>/dev/null)
-if [[ $(echo $res|awk -F "\r" '{print $NF}') -eq 200 ]]; then
-  EXPLORER=$(echo $res|awk -F "\r" '{$NF=""; print $0}')
+if [[ $(echo $res|awk '{print $NF}') -eq 200 ]]; then
+  EXPLORER=$(echo $res|awk '{print $1}')
 else
   EXPLORER="problem"
 fi
