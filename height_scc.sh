3d58d88694724d54d896a71e9e91d11102a9f5d3
Mark Brooker <mark@walkjivefly.com>
2023-01-21 22:47:36 +0100

    Handle SCC MN

--- height_scc.sh
+++ height_scc.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
+CONFIG="${HOME}/.stakecubecoin/stakecubecoin.conf"
+NODETYPE=$(awk -F= '/^masternodeblsprivkey=/ {print $1}' ${CONFIG})
 LOCAL=$(scc-cli getblockcount)
-if [[ $? -ne 0 ]]; then
+if [[ $? -eq 0 ]]; then
+  if [[ -n ${NODETYPE} ]]; then
+    MNSTATUS=$(scc-cli masternode status)
+    status=$(echo $MNSTATUS|jq -r .status)
+    penalty=$(echo $MNSTATUS|jq -r .dmnState.PoSePenalty)
+    banned=$(echo $MNSTATUS|jq -r .dmnState.PoSeBanHeight)
+    if [[ $penalty != "0" || $banned != "-1" ]]; then
+      LOCAL="PoSe"
+    elif [[ "$status" != "Ready" ]]; then
+      LOCAL=$status
+    fi
+  fi
+else
   LOCAL="problem"
 fi
 EXPLORER="http://79.143.186.234/api/"                   # Iquidus alternative
